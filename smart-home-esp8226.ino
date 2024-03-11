b8931e795acbd1a73ea6aa1857dbbf06f7a1e917
SamuelSlavka <slavka.samuel@gmail.com>
2024-03-11 22:53:03 +0100

    esp32 support

--- smart-home-esp8226.ino
+++ smart-home-esp8226.ino
@@ -1,20 +1,27 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESPAsyncWebSrv.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <HTTPClient.h>
-#include <WiFiMulti.h>
-
-#include "ESPAsyncWebServer.h"
 
 #include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #include "const.h"
 #include "DHT.h"
 
 #define DHTTYPE DHT22
 
-uint8_t DHTPin = 15;
+uint8_t DHTPin = 12;
 DHT dht(DHTPin, DHTTYPE);
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 32  // OLED display height, in pixels
+
+#define OLED_RESET -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 AsyncWebServer server(80);
 
@@ -22,8 +29,8 @@ float temperature;
 float humidity;
 
 unsigned long lastTime = 0;
-// 15 m
-unsigned long timerDelay = 900000;
+// 30 s
+unsigned long timerDelay = 30000;
 
 void connectToWiFi() {
   WiFi.mode(WIFI_STA);  // Optional
@@ -43,7 +50,7 @@ void connectToWiFi() {
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     AsyncResponseStream *response = request->beginResponseStream("application/json");
-    JsonDocument json;
+    DynamicJsonDocument json(1024);
     json["temperature"] = temperature;
     json["humidity"] = humidity;
     serializeJson(json, *response);
@@ -51,6 +58,41 @@ void connectToWiFi() {
   });
 
   server.begin();
+}
+
+
+void setupDisplay() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+
+  display.display();
+  delay(100);  // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+  display.display();
+  display.setTextSize(1.75);
+  display.setTextColor(WHITE);
+}
+
+void displayData() {
+  display.clearDisplay();
+  delay(100);
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.print("tmp: ");
+  display.print(temperature, 1);
+  display.print(" ");
+
+  display.setCursor(0, 17);
+  display.print("hum: ");
+  display.print(humidity, 1);
+  display.print(" ");
+
+  display.display();
 }
 
 void getData() {
@@ -87,12 +129,14 @@ void sendData() {
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  setupDisplay();
   connectToWiFi();
 }
 
 void loop() {
   if ((millis() - lastTime) > timerDelay) {
     getData();
+    displayData();
     sendData();
 
     Serial.print(F("Humidity: "));
